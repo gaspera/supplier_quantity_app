5a310f03b918c3043a337943b07b8d43a4caf609
gasper.antolin <gasper.antolin@inden.si>
2023-04-06 18:42:11 +0200

    Remove unneeded code

--- WarehouseServerApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WarehouseServerApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
